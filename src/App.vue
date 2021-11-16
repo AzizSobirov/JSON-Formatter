ec11c3c27a3e1ce05419fa3038fd58231456c880
Aziz Sobirov <azizsobirovdev@gmail.com>
2021-11-16 19:27:27 +0500

    app

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <form action="" v-on:submit.prevent="loadJson()">
+    <div class="content">
+      <textarea name="" v-model="input" placeholder="Enter your JSON file."></textarea>
+      <textarea name="" v-text="result" placeholder="Your Formated JSON file goes here."></textarea>
+    </div>
+    <div class="btn">
+      <button type="submit" class="as_btn"><a href="#">Format</a></button>
+      <button type="reset" class="as_btn"><a href="#">Reset</a></button>
+    </div>
+  </form>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import './css/style.css'
+export default {
+  name: 'App',
+  data() {
+    return{
+      input:'',
+      result:''
+    }
+  },
+  methods: {
+    loadJson(){
+      this.result = JSON.stringify(JSON.parse(this.input),null,4)
+      return false;
+    }
+  },
+}
+</script>
